04c9a4ffbe34f5a10100a671b6991cf8b478056d
petra_bodman <petra_bodman@hotmail.com>
2018-03-19 16:15:48 +0100

    Refactory

--- lib/atm.rb
+++ lib/atm.rb
@@ -7,12 +7,21 @@ class Atm
 
   def withdraw(amount, account)
     case
-    when amount > account.balance
+    when insufficient_funds_in_account?(amount, account)
       return
     else
+      perform_transection(amount, account)
+    end
+  end
+
+  private
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def perform_transection(amount, account)
      @funds -= amount
      account.balance = account.balance - amount
      {status: true, message: 'success', date: Date.today, amount: amount}
    end
   end
-end
