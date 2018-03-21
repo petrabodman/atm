c9b019a1e0403e56c06809b55f1e3b52f0d493e1
petra_bodman <petra_bodman@hotmail.com>
2018-03-21 10:36:02 +0100

    wip person added

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,14 +2,14 @@ require './lib/person'
 require './lib/atm'
 
 describe Person do
-  subject {described_class.new(name: 'Petra')}
+  subject { described_class.new(name: 'Petra') }
 
   it 'is expeced to have a :name on initialize' do
     expect(subject.name).not_to be nil
   end
 
   it 'is expeced to raise error if no name is set' do
-    expect {described_class.new}.to raise_error
+    expect { described_class.new }.to raise_error
     'A name is required'
   end
 
@@ -24,7 +24,7 @@ end
 
 
   describe 'can create an Account' do
-    before {subject.create_account}
+    before { subject.create_account }
     it 'of Account class' do
       expect(subject.account).to be_an_instance_of Account
     end
@@ -33,16 +33,41 @@ end
     end
   end
   describe 'can manage funds if an account been created' do
-    let(:atm) {Atm.new}
-    before {subject.create_account}
+    let(:atm) { Atm.new }
+    before { subject.create_account }
     it 'can deposit funds' do
       expect(subject.deposit(100)).to be_truthy
     end
+
+    it 'funds are added to the account balance - deducted from cash' do
+      subject.cash = 100
+      subject.deposit(100)
+      expect(subject.account.balance).to be 100
+      expect(subject.cash).to be 0
+    end
+
+    it 'can withdraw funds' do
+      command = lambda { subject.withdraw(amount: 100, pin(subject.account.pin_code, account: subject.account, atm: atm)) }
+        expect(command.call).to be_truthy
+    end
+
+    it 'withdraw is expected to raise error if no ATM is passed in' do
+      command = lambda { subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account) }
+        expect { command.call }.to raise_error 'An ATM is required'
+    end
+
+    it 'funds are added to cash - deducted from account balance' do
+      subject.cash = 100
+      subject.deposit(100)
+      subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account, atm: atm)
+        expect(subject.account.balance).to be 0
+        expect(subject.cash).to be 100
+    end
+
     describe 'can not manage funds if no
     account been created' do
       it 'can\'t deposit funds' do
-        expect {subject.deposit(100)}.to raise_error
-        (RuntimeError, 'No account present')
+        expect { subject.deposit(100) }.to raise_error(RuntimeError, 'No account present')
     end
   end
 end
